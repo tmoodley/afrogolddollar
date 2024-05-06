3e1379f01ecbb5d2066ae60a477eb39792b8e22f
Ty moodley <ty.moodley@gmail.com>
2024-05-06 09:53:09 -0400

    initial

---
+++ components/WhoWeAre/WhoWeAre.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+
+const WhoWeAre = () => {
+  return (
+    <section id="about" className="about-area pt-130 pb-130">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6">
+            <div className="about-img wow fadeInLeft" data-wow-delay=".2s">
+              <img src={"/img/images/about_img01.png"} alt="" />
+              <img
+                src={"/img/images/about_img02.png"}
+                alt=""
+                className="img-two"
+              />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="about-content wow fadeInRight" data-wow-delay=".2s">
+              <div className="section-title mb-30">
+                <span className="sub-title">Who we are</span>
+                <h2 className="title">
+                  The World’s <span>1st ICO</span> Platform That Offers Rewards
+                </h2>
+              </div>
+              <p>
+                The World’s 1st ICO Platform That Offers Rewards and The
+                platform helps investors to make easy to purchase and sell their
+                tokens
+              </p>
+              <Link href="/" className="btn">
+                Purchase Tokens
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhoWeAre;
